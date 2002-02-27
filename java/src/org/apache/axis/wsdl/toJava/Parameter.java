5d5d46109eb4de4ed07a7c62ed1806950ddbee28
Tom Jordahl <tomj@apache.org>
2002-02-27 00:12:40 +0000

    Not-ready-for-prime-time document literal work.
    Creating a branch so Tom and I can get this working at the interop event
    and check it in later.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/webservices/axis1/branches/TomAndGlen@246510 13f79535-47bb-0310-9956-ffa450edef68

--- java/src/org/apache/axis/wsdl/toJava/Parameter.java
+++ java/src/org/apache/axis/wsdl/toJava/Parameter.java
@@ -54,6 +54,7 @@
  */
 package org.apache.axis.wsdl.toJava;
 
+import javax.wsdl.QName;
 import java.util.Vector;
 
 /**
@@ -66,12 +67,28 @@ public class Parameter {
     public static final byte OUT = 2;
     public static final byte INOUT = 3;
 
-    public String name;
+    private QName name;
     public TypeEntry type;
     public byte mode = IN;
 
     public String toString() {
-        return "(" + type + ", " + name + ", "
+        return "(" + type + ", " + getName() + ", "
                 + (mode == IN ? "IN)" : mode == INOUT ? "INOUT)" : "OUT)");
     } // toString
+
+    public QName getQName() {
+        return name;
+    }
+
+    public String getName() {
+        return name.getLocalPart();
+    }
+
+    public void setName(String name) {
+        this.name = new QName("", name);
+    }
+
+    public void setQName(QName name) {
+        this.name = name;
+    }
 } // class Parameter
