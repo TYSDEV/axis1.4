71eb95284eacb824311a4e3ab70b4ea044483239
Glen Daniels <gdaniels@apache.org>
2002-10-07 03:49:39 +0000

    Complex doc/lit test code framework (still needs implementing)
    git-svn-id: https://svn.apache.org/repos/asf/webservices/axis1/branches/interop4@248374 13f79535-47bb-0310-9956-ffa450edef68

---
+++ java/test/wsdl/interop4/groupH/complexDocLit/ComplexDocLitBindingImpl.java
@@ -0,0 +1,31 @@
+/**
+ * ComplexDocLitBindingImpl.java
+ *
+ * This file was auto-generated from WSDL
+ * by the Apache Axis WSDL2Java emitter.
+ */
+
+package test.wsdl.interop4.groupH.complexDocLit;
+
+public class ComplexDocLitBindingImpl implements test.wsdl.interop4.groupH.complexDocLit.ComplexDocLitPortType{
+    public test.wsdl.interop4.groupH.complexDocLit.EchoSOAPStructFaultResponse echoSOAPStructFault(test.wsdl.interop4.groupH.complexDocLit.SOAPStruct param) throws java.rmi.RemoteException, test.wsdl.interop4.groupH.complexDocLit.SOAPStructFault {
+        return null;
+    }
+
+    public test.wsdl.interop4.groupH.complexDocLit.EchoBaseStructFaultResponse echoBaseStructFault(test.wsdl.interop4.groupH.complexDocLit.BaseStruct param) throws java.rmi.RemoteException, test.wsdl.interop4.groupH.complexDocLit.BaseStruct {
+        return null;
+    }
+
+    public test.wsdl.interop4.groupH.complexDocLit.EchoExtendedStructFaultResponse echoExtendedStructFault(test.wsdl.interop4.groupH.complexDocLit.ExtendedStruct param) throws java.rmi.RemoteException, test.wsdl.interop4.groupH.complexDocLit.ExtendedStruct {
+        return null;
+    }
+
+    public test.wsdl.interop4.groupH.complexDocLit.EchoMultipleFaults1Response echoMultipleFaults1(test.wsdl.interop4.groupH.complexDocLit.EchoMultipleFaults1Request param) throws java.rmi.RemoteException, test.wsdl.interop4.groupH.complexDocLit.BaseStruct, test.wsdl.interop4.groupH.complexDocLit.SOAPStructFault {
+        return null;
+    }
+
+    public test.wsdl.interop4.groupH.complexDocLit.EchoMultipleFaults2Response echoMultipleFaults2(test.wsdl.interop4.groupH.complexDocLit.EchoMultipleFaults2Request param) throws java.rmi.RemoteException, test.wsdl.interop4.groupH.complexDocLit.MoreExtendedStruct, test.wsdl.interop4.groupH.complexDocLit.ExtendedStruct, test.wsdl.interop4.groupH.complexDocLit.BaseStruct {
+        return null;
+    }
+
+}
