5dcd0514f42d18f66d6e24aa7a2b9058dad1b202
Damitha N.M. Kumarage <damitha@apache.org>
2003-11-28 05:30:35 +0000

    I moved all the samples to $AXISCPP_HOME/samples folder because as we discussed it is a good move
    git-svn-id: https://svn.apache.org/repos/asf/webservices/axis1/branches/Release-2003_10_26-bugfixes_branch@249960 13f79535-47bb-0310-9956-ffa450edef68

---
+++ c/samples/server/interoptests/base/InteropTestPortType.h
@@ -0,0 +1,40 @@
+/////////////////////////////////////////////////////////////////////////////
+// This is the Service Class genarated by the tool WSDL2Ws
+//		InteropTestPortType.h: interface for the InteropTestPortTypeclass.
+//
+//////////////////////////////////////////////////////////////////////
+#if !defined(__INTEROPTESTPORTTYPE_SERVERSKELETON_H__INCLUDED_)
+#define __INTEROPTESTPORTTYPE_SERVERSKELETON_H__INCLUDED_
+
+#include <axis/common/AxisUserAPI.h>
+
+#include "SOAPStruct.h"
+#include "ArrayOffloat.h"
+#include "ArrayOfSOAPStruct.h"
+#include "ArrayOfint.h"
+#include "ArrayOfstring.h"
+
+class InteropTestPortType 
+{
+	public:
+		InteropTestPortType();
+	public:
+		virtual ~InteropTestPortType();
+	public: 
+		string echoString(string Value0);
+		ArrayOfstring echoStringArray(ArrayOfstring Value0);
+		int echoInteger(int Value0);
+		ArrayOfint echoIntegerArray(ArrayOfint Value0);
+		float echoFloat(float Value0);
+		ArrayOffloat echoFloatArray(ArrayOffloat Value0);
+		SOAPStruct* echoStruct(SOAPStruct* Value0);
+		ArrayOfSOAPStruct echoStructArray(ArrayOfSOAPStruct Value0);
+		void  echoVoid();
+		Axis_Base64Binary echoBase64(Axis_Base64Binary Value0);
+		Axis_DateTime echoDate(Axis_DateTime Value0);
+		Axis_HexBinary echoHexBinary(Axis_HexBinary Value0);
+		Axis_Decimal echoDecimal(Axis_Decimal Value0);
+		Axis_Boolean echoBoolean(Axis_Boolean Value0);
+};
+
+#endif // !defined(__INTEROPTESTPORTTYPE_SERVERSKELETON_H__INCLUDED_)
