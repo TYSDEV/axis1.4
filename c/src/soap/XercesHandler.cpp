2f9703793664d29605696822a3c37116d68fa3da
Damitha N.M. Kumarage <damitha@apache.org>
2004-04-30 04:19:46 +0000

    Fixed memeory leak bug by applying the patch sent by Samisa Abeysinghe
    git-svn-id: https://svn.apache.org/repos/asf/webservices/axis1/branches/Release1-1-bugfixes-branch@257957 13f79535-47bb-0310-9956-ffa450edef68

--- c/src/soap/XercesHandler.cpp
+++ c/src/soap/XercesHandler.cpp
@@ -30,31 +30,60 @@ XercesHandler::XercesHandler()
 {
     m_nStatus = AXIS_SUCCESS;
     Nelement = (AnyElement*)malloc(sizeof (AnyElement));
+    //initilize Nelement members
+    Nelement->m_pchNameOrValue = NULL;
+    Nelement->m_pchNamespace = NULL;
+    for( int i = 0; i < MAX_NO_OF_ATTRIBUTES*3; i++)
+        Nelement->m_pchAttributes[i] = NULL;
 }
 
 XercesHandler::~XercesHandler()
 {
+    //free Nelement members
+    if(Nelement->m_pchNameOrValue)
+        XMLString::release((const_cast<char **>(&(Nelement-> m_pchNameOrValue))));
+    if(Nelement->m_pchNamespace)
+        XMLString::release((const_cast<char **>(&(Nelement-> m_pchNamespace))));
+    for( int i = 0; i < MAX_NO_OF_ATTRIBUTES*3; i++)
+    {
+        if(Nelement->m_pchAttributes[i])
+            XMLString::release((const_cast<char **>(&(Nelement-> m_pchAttributes[i]))));
+    }
+    //free Nelement
+    free(Nelement);
 }
 
-void XercesHandler::startElement(const XMLCh *const uri,const XMLCh *const 
+void XercesHandler::startElement(const XMLCh *const uri,const XMLCh *const
                                  localname,const XMLCh *const qname,
                                  const Attributes &attrs)
 {
     m_pCurrElement = Nelement;
     Nelement->m_type = START_ELEMENT;
+    //free alredy allocated transcodes
+    if(Nelement->m_pchNameOrValue)
+        XMLString::release((const_cast<char **>(&(Nelement-> m_pchNameOrValue))));
+    if(Nelement->m_pchNamespace)
+        XMLString::release((const_cast<char **>(&(Nelement-> m_pchNamespace))));
+
     Nelement->m_pchNameOrValue = XMLString::transcode(localname);
     Nelement->m_pchNamespace = XMLString::transcode(uri);
-    
+
     unsigned int len = attrs.getLength();
     unsigned int index;
 	unsigned int i = 0;
     for (index = 0; index < len*3; index+=3)
-    {    
-        Nelement->m_pchAttributes[index] = 
+    {
+        if(Nelement->m_pchAttributes[index])
+            XMLString::release((const_cast<char **>(&(Nelement-> m_pchAttributes[index]))));
+        Nelement->m_pchAttributes[index] =
             XMLString::transcode(attrs.getLocalName(i));
-        Nelement->m_pchAttributes[index+1] = 
+        if(Nelement->m_pchAttributes[index])
+            XMLString::release((const_cast<char **>(&(Nelement-> m_pchAttributes[index+1]))));
+        Nelement->m_pchAttributes[index+1] =
             XMLString::transcode(attrs.getURI(i));
-        Nelement->m_pchAttributes[index+2] = 
+        if(Nelement->m_pchAttributes[index])
+            XMLString::release((const_cast<char **>(&(Nelement-> m_pchAttributes[index + 2]))));
+        Nelement->m_pchAttributes[index+2] =
             XMLString::transcode(attrs.getValue(i));
 		i++;
     }
@@ -70,14 +99,16 @@ const XML_Ch* XercesHandler::NS4Prefix(const XML_Ch* prefix)
     return NULL;
 }
 
-void XercesHandler::characters(const XMLCh* const chars, 
+void XercesHandler::characters(const XMLCh* const chars,
                                const unsigned int length)
 {
     m_pCurrElement = Nelement;
     Nelement->m_type = CHARACTER_ELEMENT;
+    if(Nelement->m_pchNameOrValue)
+        XMLString::release((const_cast<char **>(&(Nelement-> m_pchNameOrValue))));
     Nelement->m_pchNameOrValue = XMLString::transcode(chars);
 }
-void XercesHandler::ignorableWhitespace(const XMLCh* const chars, 
+void XercesHandler::ignorableWhitespace(const XMLCh* const chars,
                                         const unsigned int length)
 {}
 void XercesHandler::resetDocument()
@@ -101,25 +132,41 @@ void XercesHandler::endElement (const XMLCh *const uri,
 {
     m_pCurrElement = Nelement;
     Nelement->m_type = END_ELEMENT;
+    if(Nelement->m_pchNameOrValue)
+        XMLString::release((const_cast<char **>(&(Nelement-> m_pchNameOrValue))));
+    if(Nelement->m_pchNamespace)
+        XMLString::release((const_cast<char **>(&(Nelement-> m_pchNamespace))));
     Nelement->m_pchNameOrValue = XMLString::transcode(localname);
     Nelement->m_pchNamespace = XMLString::transcode(uri);
+    if(Nelement->m_pchAttributes[0])
+        XMLString::release((const_cast<char **>(&(Nelement->m_pchAttributes[0]))));
     Nelement->m_pchAttributes[0] = NULL;
 }
 
-void XercesHandler::startPrefixMapping(const XMLCh* const prefix, 
+void XercesHandler::startPrefixMapping(const XMLCh* const prefix,
                                        const XMLCh* const uri)
 {
     Nelement->m_type = START_PREFIX;
+    if(Nelement->m_pchNameOrValue)
+        XMLString::release((const_cast<char **>(&(Nelement-> m_pchNameOrValue))));
+    if(Nelement->m_pchNamespace)
+        XMLString::release((const_cast<char **>(&(Nelement-> m_pchNamespace))));
+
     Nelement->m_pchNameOrValue = XMLString::transcode(prefix);
     Nelement->m_pchNamespace = XMLString::transcode(uri);
-    m_NsStack[XMLString::transcode(prefix)] = XMLString::transcode(uri);
+    //do not use transcodein this statement
+    //m_NsStack[XMLString::transcode(prefix)] = XMLString::transcode(uri);
+    m_NsStack[Nelement->m_pchNameOrValue] = Nelement->m_pchNamespace;
+
 }
 
 void XercesHandler::endPrefixMapping(const XMLCh* const prefix)
 {
     Nelement->m_type = END_PREFIX;
+    if(Nelement->m_pchNameOrValue)
+        XMLString::release((const_cast<char **>(&(Nelement-> m_pchNameOrValue))));
     Nelement->m_pchNameOrValue = XMLString::transcode(prefix);
-    m_NsStack.erase(XMLString::transcode(prefix));
+    m_NsStack.erase(Nelement->m_pchNameOrValue);
 }
 
 void XercesHandler::freeElement()
