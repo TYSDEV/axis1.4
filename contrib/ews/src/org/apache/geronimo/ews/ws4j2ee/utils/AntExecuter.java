ed9795a676b26c55d2fdcb6552dbf388c8b2fbb2
Hemapani Srinath Perera <hemapani@apache.org>
2004-09-02 11:48:29 +0000

    break the AntExecutor code to small methods
    git-svn-id: https://svn.apache.org/repos/asf/webservices/axis1/trunk@259106 13f79535-47bb-0310-9956-ffa450edef68

--- contrib/ews/src/org/apache/geronimo/ews/ws4j2ee/utils/AntExecuter.java
+++ contrib/ews/src/org/apache/geronimo/ews/ws4j2ee/utils/AntExecuter.java
@@ -17,16 +17,21 @@
 package org.apache.geronimo.ews.ws4j2ee.utils;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 import java.util.Vector;
 import java.util.jar.JarOutputStream;
 
 import org.apache.geronimo.ews.ws4j2ee.context.J2EEWebServiceContext;
+import org.apache.geronimo.ews.ws4j2ee.toWs.GenerationConstants;
 import org.apache.geronimo.ews.ws4j2ee.toWs.GenerationFault;
 import org.apache.geronimo.ews.ws4j2ee.utils.packager.ModulePackager;
 import org.apache.tools.ant.Project;
 import org.apache.tools.ant.taskdefs.Ant;
 import org.apache.tools.ant.taskdefs.Javac;
+import org.apache.tools.ant.types.FileSet;
 import org.apache.tools.ant.types.Path;
 
 /**
@@ -70,87 +75,16 @@ public class AntExecuter{
 //			"directory in the conf/ws4j2ee.propertites Build fill ignore the faliure");
         }
     }
+    
+    
+    
     public void execute()throws GenerationFault {
         try{
             Class.forName("com.sun.tools.javac.Main");
             File outDir = new File(wscontext.getMiscInfo().getOutPutPath());
             File dest = new File(outDir,"build/classes");
-            
-            Project project = new Project();
-            project.init();
-            project.setCoreLoader(Thread.currentThread().getContextClassLoader());
-                
-                
-            Javac comp = new Javac();
-            comp.setProject(project);
-            
-            dest.mkdirs();
-            comp.setDestdir(dest);
-                
-            Path path = new Path(project);
-            path.setLocation(outDir);
-            comp.setSrcdir(path);
-
-                        
-            comp.init();
-            comp.execute();
-        
-            String jarName = wscontext.getWSDLContext().getTargetPortType().getName().toLowerCase();
-            int index = jarName.lastIndexOf(".");
-            if(index>0){
-                jarName = jarName.substring(index+1);
-            } 
-
-        
-            ModulePackager module = new ModulePackager(new File(outDir,jarName+"-ewsImpl.jar"));
-            module.addClassFiles(dest);
-            
-            Vector classpathelements = wscontext.getMiscInfo().getClasspathElements();
-            if(classpathelements != null){
-                for(int i = 0;i<classpathelements.size();i++){
-                    module.addJarFile((File)classpathelements.get(i));               
-                }
-            }
-
-            File dir = outDir;
-            File[] files = dir.listFiles();
-            if(files != null){
-                for(int i = 0;i<files.length;i++){
-                    String file = files[i].getName();
-                    if(files[i].isFile() && 
-                        !(file.endsWith(".jar")||
-                            file.endsWith(".zip")||
-                            file.endsWith(".war")||
-                            file.endsWith(".ear")||
-                            file.endsWith(".java"))
-                        ){
-                            module.addFileToJar(file,files[i]);
-                    }
-                        
-                }
-            }
-                
-            dir = new File(outDir,"META-INF");
-            files = dir.listFiles();
-            if(files != null){
-                for(int i = 0;i<files.length;i++){
-                    String file = files[i].getName();
-                    if(files[i].isFile() && 
-                        !(file.endsWith(".jar")||
-                            file.endsWith(".zip")||
-                            file.endsWith(".war")||
-                            file.endsWith(".ear")||
-                            file.endsWith(".java"))
-                        ){
-                            module.addFileToJar("META-INF/"+file,files[i]);
-                    }
-                        
-                }
-            }
-
-            module.finalizeJar();
-            
-            
+            compile(outDir,dest);
+            createModule(outDir,dest);
         }catch(IOException e){
             throw GenerationFault.createGenerationFault(e);
         }catch(ClassNotFoundException e){
@@ -165,6 +99,144 @@ public class AntExecuter{
 //          "if it is a compile error you may not have set the maven reposiroty " +
 //          "directory in the conf/ws4j2ee.propertites Build fill ignore the faliure");
         }
+    }
+
+    private void createModule(File outDir,File dest)throws IOException{
+        String jarName = wscontext.getWSDLContext().getTargetPortType().getName().toLowerCase();
+        int index = jarName.lastIndexOf(".");
+        if(index>0){
+            jarName = jarName.substring(index+1);
+        } 
+
+        
+        ModulePackager module = new ModulePackager(new File(outDir,jarName+"-ewsImpl.jar"));
+        module.addClassFiles(dest);
+            
+        Vector classpathelements = wscontext.getMiscInfo().getClasspathElements();
+        if(classpathelements != null){
+            for(int i = 0;i<classpathelements.size();i++){
+                module.addJarFile((File)classpathelements.get(i));               
+            }
+        }
+
+        File dir = outDir;
+        File[] files = dir.listFiles();
+        if(files != null){
+            for(int i = 0;i<files.length;i++){
+                String file = files[i].getName();
+                if(files[i].isFile() && 
+                    !(file.endsWith(".jar")||
+                        file.endsWith(".zip")||
+                        file.endsWith(".war")||
+                        file.endsWith(".ear")||
+                        file.endsWith(".java"))
+                    ){
+                        module.addFileToJar(file,files[i]);
+                }
+                        
+            }
+        }
+                
+        dir = new File(outDir,"META-INF");
+        files = dir.listFiles();
+        if(files != null){
+            for(int i = 0;i<files.length;i++){
+                String file = files[i].getName();
+                if(files[i].isFile() && 
+                    !(file.endsWith(".jar")||
+                        file.endsWith(".zip")||
+                        file.endsWith(".war")||
+                        file.endsWith(".ear")||
+                        file.endsWith(".java"))
+                    ){
+                        module.addFileToJar("META-INF/"+file,files[i]);
+                }
+                        
+            }
+        }
+
+        module.finalizeJar();
+    
+    }
+    private void compile(File outDir,File destDir) throws IOException{
+        Project project = new Project();
+        project.init();
+        project.setCoreLoader(Thread.currentThread().getContextClassLoader());
+                
+                
+        Javac comp = new Javac();
+        comp.setProject(project);
+            
+            
+        destDir.mkdirs();
+        comp.setDestdir(destDir);
+                
+        Path path = new Path(project);
+        path.setLocation(outDir);
+        comp.setSrcdir(path);
+        
+        File repository = findMavenRepository();
+        if(repository != null){
+            Path compileClasspath = new Path(project);
+            FileSet fileset = new FileSet();
+            fileset.setDir(repository);
+            fileset.setIncludes("axis/**/*.jar");
+            fileset.setIncludes("geronimo-spec/**/*.jar");
+            fileset.setIncludes("geronimo/**/*.jar");
+            fileset.setIncludes("sec/**/*.jar");
+            fileset.setIncludes("dom4j/**/*.jar");
+            fileset.setIncludes("jaxb-ri/**/*.jar");
+            fileset.setIncludes("xerces/**/*.jar");
+            fileset.setIncludes("ews/**/*.jar");
+            fileset.setIncludes("openejb/**/*.jar");
+            compileClasspath.addFileset(fileset);
+            comp.setClasspath(compileClasspath);
+        }else{
+            
+        }
+                        
+        comp.init();
+        comp.execute();
+    
+    }
+    
+    private File findMavenRepository() throws IOException{
+        Properties pro = new Properties();
+        InputStream prpertyIn = null;
+        File file = new File(GenerationConstants.WS4J2EE_PROPERTY_FILE);
+        if(file.exists()){
+            prpertyIn = new FileInputStream(file);
+        }else{
+            file = new File("modules/axis/target/"+GenerationConstants.WS4J2EE_PROPERTY_FILE);
+            if(file.exists()){
+                prpertyIn = new FileInputStream(file);
+            }else{
+                file = new File("target/"+GenerationConstants.WS4J2EE_PROPERTY_FILE);
+                if(file.exists()){
+                    prpertyIn = new FileInputStream(file);
+                }else{
+                    prpertyIn = getClass().getClassLoader().getResourceAsStream(GenerationConstants.WS4J2EE_PROPERTY_FILE);
+                }               
+            }
+        }
+        if(prpertyIn!= null){
+            String location = null;
+            try{
+                pro.load(prpertyIn);
+                location = pro.getProperty(GenerationConstants.MAVEN_LOCAL_REPOSITARY);
+            }finally{
+                prpertyIn.close();
+            }
+            if(location != null){
+                File locationFile = new File(location);
+                if(locationFile.exists()){
+                    return locationFile;
+                }
+            }
+            
+
+        }
+        return null;
     }
     
     
