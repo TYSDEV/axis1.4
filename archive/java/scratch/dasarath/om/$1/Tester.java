1e2d8f219341b013fd5492649fe0e3d4b1ea7c7a
Dasarath Weeratunge <dasarath@apache.org>
2005-03-23 07:48:31 +0000

    git-svn-id: https://svn.apache.org/repos/asf/webservices/axis/trunk@158744 13f79535-47bb-0310-9956-ffa450edef68

--- archive/java/scratch/dasarath/om/$1/Tester.java
+++ archive/java/scratch/dasarath/om/$1/Tester.java
@@ -5,43 +5,67 @@
 
 /**
  * @author Dasarath
- *
+ * 
  * @date Dec 30, 2004
  */
 public class Tester {
-	public static final long N= 200;
-	public static final long M= 10000;
-	public static final long T= 100;
-	void test1() throws Exception {
-		for (int i= 0; i < N; i++) {
-			LLElement e= new LLElement();
-			for (int j= 0; j < M; j++)
-				e.addChild(new LLElement());
-			for (int k= 0; k < T; k++)
-				e.traverse();			
-		}
+	public static final long N = 200;
+
+	private long numChildren;
+
+	private long numTraversals;
+
+	Tester(long c, long t) {
+		numChildren = c;
+		numTraversals = t;
 	}
 
-	void test2() throws Exception {
-		for (int i= 0; i < N; i++) {
-			ALElement e= new ALElement();
-			for (int j= 0; j < M; j++)
-				e.addChild(new ALElement());
-			for (int k= 0; k < T; k++)
+	void testLLElement() throws Exception {
+		for (int i = 0; i < N; i++) {
+			LLElement e = new LLElement();
+			for (int j = 0; j < numChildren; j++)
+				e.addChild(new LLElement());
+			for (int k = 0; k < numTraversals; k++)
 				e.traverse();
 		}
 	}
 
-	public static void main(String[] args) {
-		try {
-			long t1= System.currentTimeMillis();
-			new Tester().test2();
-			long t2= System.currentTimeMillis();
-			System.out.println((t2 - t1)/N);
+	void testALElement(boolean useIter) throws Exception {
+		for (int i = 0; i < N; i++) {
+			ALElement e = new ALElement();
+			for (int j = 0; j < numChildren; j++)
+				e.addChild(new ALElement());
+			for (int k = 0; k < numTraversals; k++)
+				e.traverse(useIter);
 		}
-		catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+	}
+
+	private static void test(long c, long t, boolean useIter) throws Exception {
+		Tester instance = new Tester(c, t);
+		System.out.println("children= " + c + ", traversals= " + t
+				+ "using iterator= " + useIter);
+		long t1 = System.currentTimeMillis();
+		instance.testLLElement();
+		long t2 = System.currentTimeMillis();
+		long r = (t2 - t1) / N;
+		System.out.println("LLElement= " + r + "ms");
+		t1 = System.currentTimeMillis();
+		instance.testALElement(useIter);
+		t2 = System.currentTimeMillis();
+		r = (t2 - t1) / N;
+		System.out.println("ALElement= " + r + "ms");
+	}
+
+	public static void main(String[] args) throws Exception {
+		test(1000, 1, false);
+		test(10000, 1, false);
+
+		test(100, 100, false);
+		test(100, 1000, false);
+		test(100, 10000, false);
+
+		test(100, 100, true);
+		test(100, 1000, true);
+		test(100, 10000, true);
 	}
 }
