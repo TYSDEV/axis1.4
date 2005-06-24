463f9eadfd640465f30917c8f21d122b041eb412
Venkat Reddy <venkat@apache.org>
2005-06-24 06:45:53 +0000

    Cleaning up of the stuff added by Eclipse code templates and adding Apache license.
    git-svn-id: https://svn.apache.org/repos/asf/webservices/axis/trunk@201571 13f79535-47bb-0310-9956-ffa450edef68

--- archive/java/scratch/ashu_jaya_venkat/JAX-WS2/javax/xml/rpc/handler/AbstractHandler.java
+++ archive/java/scratch/ashu_jaya_venkat/JAX-WS2/javax/xml/rpc/handler/AbstractHandler.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2004,2005 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package javax.xml.rpc.handler;
 
 import javax.xml.rpc.ProtocolException;
@@ -14,39 +30,39 @@ import javax.xml.rpc.ProtocolException;
  */
 public interface AbstractHandler<C extends MessageContext> extends HandlerLifecycle {
 
-	/**
-	 * The handleMessage method is invoked for normal processing of inbound and outbound messages. Refer to the
-	 * description of the handler framework in the JAX-RPC 2.0 specification for full details.
-	 * 
-	 * @param context - the message context.
-	 * @return An indication of whether handler processing should continue for the current message
-	 *  - Return true to continue processing.
-	 *  - Return false to block processing.
-	 * @throws java.lang.RuntimeException - Causes the JAX-RPC runtime to cease handler processing and generate a
-	 * fault.
-	 * @throws ProtocolException - Causes the JAX-RPC runtime to switch to fault message processing.
-	 */
-	boolean handleMessage(C context) throws java.lang.RuntimeException, ProtocolException;
-	
-	/**
-	 * The handleFault method is invoked for fault message processing. Refer to the description of the handler framework in
-	 * the JAX-RPC 2.0 specification for full details.
-	 * 
-	 * @param context - the message context
-	 * @return An indication of whether handler fault processing should continue for the current message
-	 *  - Return true to continue processing.
-	 *  - Return false to block processing.
-	 * @throws java.lang.RuntimeException - Causes the JAX-RPC runtime to cease handler fault processing and dispatch
-	 * the fault.
-	 * @throws ProtocolException - Causes the JAX-RPC runtime to cease handler fault processing and dispatch the fault.
-	 */
-	boolean handleFault(C context) throws java.lang.RuntimeException, ProtocolException;
-	
-	/**
-	 * Called at the conclusion of a message exchange pattern just prior to the JAX-RPC runtime disptaching a message, fault or
-	 * exception. Refer to the description of the handler framework in the JAX-RPC 2.0 specification for full details.
-	 *  
-	 * @param context - the message context
-	 */
-	void close(MessageContext context);
+    /**
+     * The handleMessage method is invoked for normal processing of inbound and outbound messages. Refer to the
+     * description of the handler framework in the JAX-RPC 2.0 specification for full details.
+     *
+     * @param context - the message context.
+     * @return An indication of whether handler processing should continue for the current message
+     *  - Return true to continue processing.
+     *  - Return false to block processing.
+     * @throws java.lang.RuntimeException - Causes the JAX-RPC runtime to cease handler processing and generate a
+     * fault.
+     * @throws ProtocolException - Causes the JAX-RPC runtime to switch to fault message processing.
+     */
+    boolean handleMessage(C context) throws java.lang.RuntimeException, ProtocolException;
+
+    /**
+     * The handleFault method is invoked for fault message processing. Refer to the description of the handler framework in
+     * the JAX-RPC 2.0 specification for full details.
+     *
+     * @param context - the message context
+     * @return An indication of whether handler fault processing should continue for the current message
+     *  - Return true to continue processing.
+     *  - Return false to block processing.
+     * @throws java.lang.RuntimeException - Causes the JAX-RPC runtime to cease handler fault processing and dispatch
+     * the fault.
+     * @throws ProtocolException - Causes the JAX-RPC runtime to cease handler fault processing and dispatch the fault.
+     */
+    boolean handleFault(C context) throws java.lang.RuntimeException, ProtocolException;
+
+    /**
+     * Called at the conclusion of a message exchange pattern just prior to the JAX-RPC runtime disptaching a message, fault or
+     * exception. Refer to the description of the handler framework in the JAX-RPC 2.0 specification for full details.
+     *
+     * @param context - the message context
+     */
+    void close(MessageContext context);
 }
