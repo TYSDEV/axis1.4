a2d9e806ed4dcae3c23b04d9802e7b10f3d9e36a
Jayachandra Sekhara Rao Sunkara <jayachandra@apache.org>
2005-06-28 15:07:09 +0000

    committing ServiceFactory implementation so far, and parallel junit test code.
    git-svn-id: https://svn.apache.org/repos/asf/webservices/axis/trunk@202218 13f79535-47bb-0310-9956-ffa450edef68

---
+++ archive/java/scratch/ashu_jaya_venkat/jaxws/test/org/apache/axis/jaxrpc/ServiceFactoryTest.java
@@ -0,0 +1,34 @@
+package org.apache.axis.jaxrpc;
+
+import junit.framework.TestCase;
+
+import javax.xml.rpc.ServiceFactory;
+import javax.xml.rpc.Service;
+import javax.xml.namespace.QName;
+
+public class ServiceFactoryTest extends TestCase {
+
+	public ServiceFactoryTest(String name) {
+		super(name);
+	}
+	
+	public void testServiceFactoryInstantiation() {
+		try {
+			ServiceFactory sf = ServiceFactory.newInstance();
+			assertTrue(sf!=null);
+		} catch (Exception e) {
+			fail(e.getMessage());
+		}
+	}
+	
+	public void testServiceCreation() {
+		try {
+			ServiceFactory sf = ServiceFactory.newInstance();
+			Service s = sf.createService(null,new QName("http://defaultPackage","Echo"));
+			assertTrue(s!=null);
+		} catch (Exception e) {
+			fail(e.getMessage());
+		}
+	}
+
+}
