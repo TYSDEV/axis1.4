f80c9fd2a3abead4be129816c9c93e12df679d81
Ashutosh Shahi <ashutosh@apache.org>
2005-06-29 14:05:22 +0000

    Implementation of JAXRPCContext, BindingProvider and related files.
    git-svn-id: https://svn.apache.org/repos/asf/webservices/axis/trunk@202378 13f79535-47bb-0310-9956-ffa450edef68

---
+++ archive/java/scratch/ashu_jaya_venkat/jaxws/src/org/apache/axis/jaxrpc/BindingProviderImpl.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2004,2005 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.axis.jaxrpc;
+
+import javax.xml.rpc.Binding;
+import javax.xml.rpc.BindingProvider;
+import javax.xml.rpc.JAXRPCContext;
+
+public class BindingProviderImpl implements BindingProvider {
+
+	protected JAXRPCRequestContext requestContext;
+	protected JAXRPCResponseContext responseContext;
+	
+	public JAXRPCContext getRequestContext() {
+		if(requestContext == null)
+			requestContext = new JAXRPCRequestContext(this);
+		return requestContext;
+	}
+
+	public JAXRPCContext getResponseContext() {
+		if(responseContext == null)
+			responseContext = new JAXRPCResponseContext(this);
+		return null;
+	}
+
+	public Binding getBinding() {
+		
+		return null;
+	}
+
+}
---
+++ archive/java/scratch/ashu_jaya_venkat/jaxws/src/org/apache/axis/jaxrpc/JAXRPCContextImpl.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright 2004,2005 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.axis.jaxrpc;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+import javax.xml.rpc.BindingProvider;
+import javax.xml.rpc.JAXRPCContext;
+import javax.xml.rpc.JAXRPCException;
+
+public class JAXRPCContextImpl implements JAXRPCContext {
+
+	protected static List<String> standardProperties;
+	protected static Map properties;
+	protected Object associatedObject;
+	
+	public JAXRPCContextImpl(BindingProvider provider){
+		
+		associatedObject = provider;
+		
+		properties = new HashMap();
+		
+		standardProperties = new ArrayList();
+		standardProperties.add("javax.xml.rpc.binding.attachments");
+		standardProperties.add("javax.xml.rpc.wsdl.description");
+		standardProperties.add("javax.xml.rpc.wsdl.service");
+		standardProperties.add("javax.xml.rpc.wsdl.port");
+		standardProperties.add("javax.xml.rpc.wsdl.interface");
+		standardProperties.add("javax.xml.rpc.wsdl.operation");
+	}
+	
+	public void setProperty(String name, Object value)
+			throws IllegalArgumentException {
+		validateProperty(name, value);
+		properties.put(name, value);
+	}
+
+	public void removeProperty(String name) throws IllegalArgumentException {
+		validateProperty(name, null);
+		properties.remove(name);
+	}
+
+	public Object getProperty(String name) throws IllegalArgumentException {
+		validateProperty(name, null);
+		return properties.get(name);
+	}
+
+	public Iterator getPropertyNames() {
+		if(properties != null)
+			return properties.keySet().iterator();
+		else
+			return null;
+	}
+	
+	private void validateProperty(String name, Object value){
+		if(name == null)
+			throw new JAXRPCException("User-defined property name can not be null");
+		if(standardProperties.indexOf(name) == -1 && name.startsWith("javax.xml.rpc"))
+			throw new JAXRPCException("User-Defined property can not start with javax.ml.rpc");
+
+	}
+
+}
