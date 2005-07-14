a664d5c2cec5108291e639bb8068470acd27f9fc
Eran Chinthaka <chinthaka@apache.org>
2005-07-14 07:57:23 +0000

    Adding SOAP 1.2 Testing stuff on behalf of Anushka, Thilakarathne and Dias
    git-svn-id: https://svn.apache.org/repos/asf/webservices/axis/trunk@219007 13f79535-47bb-0310-9956-ffa450edef68

---
+++ archive/java/scratch/Anushka_Chinthaka_Dias/SOAP12Testing/src/org/apache/axis2/soap12testing/handlers/HeaderConstants.java
@@ -0,0 +1,31 @@
+/*
+* Copyright 2001-2004 The Apache Software Foundation.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+package org.apache.axis2.soap12testing.handlers;
+
+public interface HeaderConstants {
+
+    public final String REQUEST_HEADERBLOCK_NAME = "echoOk";
+    public final String RESPONSE_HEADERBLOCK_NAME = "responseOk";
+    public final String SAMPLE_ROLE = "http://example.org/ts-tests";
+    public final String SOAP12_ROLE = "http://www.w3.org/2003/05/soap-envelope/role";
+    public final String ULTIMATERECEIVER_ROLE = "ultimateReceiver";
+    public final String NEXT_ROLE = "next";
+    public final String NONE_ROLE = "none";
+    public final String ROLE_BY_B = "B";
+    public final String ROLE_BY_C = "C";
+
+}
